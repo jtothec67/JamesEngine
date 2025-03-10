8879fd69b96f42c05c2813476b85a94967420f61
James <jamescomer03@gmail.com>
2025-03-10 13:52:33 +0000

    Started adding physics to rigidbody
    Had to change labs

--- src/JamesEngine/Rigidbody.cpp
+++ src/JamesEngine/Rigidbody.cpp
@@ -15,6 +15,11 @@ namespace JamesEngine
 
 	void Rigidbody::OnTick()
 	{
+		// Step 1: Compute each of the forces acting on the object (only gravity by default)
+		glm::vec3 force = mMass * mAcceleration;
+		AddForce(force);
+
+		// Step 2: Compute collisions
 		// Get all colliders in the scene
 		std::vector<std::shared_ptr<Collider>> colliders;
 		GetEntity()->GetCore()->FindComponents(colliders);
@@ -31,65 +36,93 @@ namespace JamesEngine
 			glm::vec3 collisionPoint;
 			glm::vec3 collisionNormal;
 			float penetrationDepth;
+
 			// Check if colliding
 			if (otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
 			{
-				std::string otherTag = otherCollider->GetEntity()->GetTag();
-				
+				// Call OnCOllision for all components on both entities
 				for (size_t ci = 0; ci < GetEntity()->mComponents.size(); ci++)
 				{
 					GetEntity()->mComponents.at(ci)->OnCollision(otherCollider->GetEntity());
 				}
 
-				std::string thisTag = GetEntity()->GetTag();
 				for (size_t ci = 0; ci < otherCollider->GetEntity()->mComponents.size(); ci++)
 				{
 					otherCollider->GetEntity()->mComponents.at(ci)->OnCollision(GetEntity());
 				}
 
-				// Kludge (*vomit emoji*)
-				float amount = 0.01f;
-				float step = 0.1f;
+				bool otherIsRigidbody = false;
+				std::shared_ptr<Rigidbody> otherRigidbody = std::dynamic_pointer_cast<Rigidbody>(otherCollider);
 
-				while (true)
-				{
-					if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
-						break;
+				if (otherRigidbody)
+					otherIsRigidbody = true;
 
-					Move(glm::vec3(amount, 0, 0));
-					if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
-						break;
 
-					Move(-glm::vec3(amount, 0, 0));
-					Move(-glm::vec3(amount, 0, 0));
-					if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
-						break;
 
-					Move(glm::vec3(amount, 0, 0));
-					Move(glm::vec3(0, 0, amount));
-					if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
-						break;
+				// Move the object out of the collision
 
-					Move(-glm::vec3(0, 0, amount));
-					Move(-glm::vec3(0, 0, amount));
-					if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
-						break;
 
-					Move(glm::vec3(0, 0, amount));
-					Move(glm::vec3(0, amount, 0));
-					if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
-						break;
+				//// Kludge (*vomit emoji*)
+				//float amount = 0.01f;
+				//float step = 0.1f;
 
-					Move(-glm::vec3(0, amount, 0));
-					Move(-glm::vec3(0, amount, 0));
-					if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
-						break;
+				//while (true)
+				//{
+				//	if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
+				//		break;
 
-					Move(glm::vec3(0, amount, 0));
-					amount += step;
-				}
+				//	Move(glm::vec3(amount, 0, 0));
+				//	if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
+				//		break;
+
+				//	Move(-glm::vec3(amount, 0, 0));
+				//	Move(-glm::vec3(amount, 0, 0));
+				//	if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
+				//		break;
+
+				//	Move(glm::vec3(amount, 0, 0));
+				//	Move(glm::vec3(0, 0, amount));
+				//	if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
+				//		break;
+
+				//	Move(-glm::vec3(0, 0, amount));
+				//	Move(-glm::vec3(0, 0, amount));
+				//	if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
+				//		break;
+
+				//	Move(glm::vec3(0, 0, amount));
+				//	Move(glm::vec3(0, amount, 0));
+				//	if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
+				//		break;
+
+				//	Move(-glm::vec3(0, amount, 0));
+				//	Move(-glm::vec3(0, amount, 0));
+				//	if (!otherCollider->IsColliding(collider, collisionPoint, collisionNormal, penetrationDepth))
+				//		break;
+
+				//	Move(glm::vec3(0, amount, 0));
+				//	amount += step;
+				//}
+
+
 			}
 		}
+	}
+
+	void Rigidbody::UpdateInertiaTensor()
+	{
+		// Calculate the inertia tensor
+		glm::mat3 inertiaTensor = glm::mat3(0);
+		glm::vec3 size = GetEntity()->GetComponent<BoxCollider>()->GetSize();
+		float x = size.x;
+		float y = size.y;
+		float z = size.z;
+
+		inertiaTensor[0][0] = mMass / 12 * (y * y + z * z);
+		inertiaTensor[1][1] = mMass / 12 * (x * x + z * z);
+		inertiaTensor[2][2] = mMass / 12 * (x * x + y * y);
+
+		mInertiaTensorInverse = glm::inverse(inertiaTensor);
 	}
 
 }
--- src/JamesEngine/Rigidbody.h
+++ src/JamesEngine/Rigidbody.h
@@ -9,6 +9,46 @@ namespace JamesEngine
 	{
 	public:
 		void OnTick();
+
+		void AddForce(glm::vec3 _force) { mForce += _force; }
+		void AddTorque(glm::vec3 _torque) { mTorque += _torque; }
+		void ClearForces() { mForce = glm::vec3(0); mTorque = glm::vec3(0); }
+
+		void SetMass(float _mass) { mMass = _mass; }
+		float GetMass() { return mMass; }
+
+		void SetForce(glm::vec3 _force) { mForce = _force; }
+		glm::vec3 GetForce() { return mForce; }
+
+		void SetVelocity(glm::vec3 _velocity) { mVelocity = _velocity; }
+		glm::vec3 GetVelocity() { return mVelocity; }
+
+		void SetAcceleration(glm::vec3 _acceleration) { mAcceleration = _acceleration; }
+		glm::vec3 GetAcceleration() { return mAcceleration; }
+
+		
+
+	private:
+
+		void UpdateInertiaTensor();
+
+		glm::vec3 mForce = glm::vec3(0);
+		glm::vec3 mVelocity = glm::vec3(0);
+		glm::vec3 mAcceleration = glm::vec3(0, -9.81f, 0);
+		glm::vec3 mPreviousPosition = glm::vec3(0);
+
+		float mMass = 1;
+
+		glm::vec3 mTorque = glm::vec3{ 0 };
+		glm::vec3 mAngularVelocity = glm::vec3{ 0 };
+		glm::vec3 mAngularMomentum = glm::vec3{ 0 };
+		glm::mat3 mInertiaTensorInverse = glm::mat3{ 0 };
+		glm::mat3 mBodyInertiaTensorInverse = glm::mat3{ 0 };
+		glm::mat3 mR = glm::mat3(	1.0f, 0.0f, 0.0f,
+									0.0f, 1.0f, 0.0f,
+									0.0f, 0.0f, 1.0f);
+
+		float globalDamping = 125;
 	};
 
 }
